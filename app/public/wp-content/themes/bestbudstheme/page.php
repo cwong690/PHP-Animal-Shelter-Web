1b4d810ea74641362f7da9d1d3454f2e292ea43f
Cindy Wong <cindywong@gmail.com>
2020-01-18 13:27:31 -0800

    template html/css added

---
+++ app/public/wp-content/themes/bestbudstheme/page.php
@@ -0,0 +1,10 @@
+<?php get_header();
+
+    while(have_posts()) {
+        the_post(); ?>
+        <h1>Page, not Post </h1>
+        <h2><?php the_title(); ?><h2>
+        <?php the_content(); ?>
+    <?php }
+    get_footer();
+?>
